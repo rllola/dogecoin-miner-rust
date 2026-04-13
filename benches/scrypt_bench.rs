5c6ff9f38aa8e05ae936ef960c51cea45b546f23
rllola <rllola80@gmail.com>
2026-04-13 09:25:40 +0200

    rustfmt

--- benches/scrypt_bench.rs
+++ benches/scrypt_bench.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
 
 // Dogecoin block header used in the test suite (test_scrypt_hasher)
 const BLOCK_HEX: &str = "01000000f615f7ce3b4fc6b8f61e8f89aedb1d0852507650533a9e3b10b9bbcc30639f279fcaa86746e1ef52d3edb3c4ad8259920d509bd073605c9bf1d59983752a6b06b817bb4ea78e011d012d59d4";
