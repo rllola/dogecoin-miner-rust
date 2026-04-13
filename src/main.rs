3400f164f8ef16bd6a6132e97bb01079cdbf42fc
rllola <rllola80@gmail.com>
2026-04-13 16:07:37 +0200

    added CLI and config file path configuration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+use clap::{arg, value_parser, Command};
+
 mod configs;
 mod miner;
 mod rpc;
 mod utils;
 
 fn main() {
-    println!("Dogecoin miner!");
+    let matches = Command::new("Miner")
+        .about("Dogecoin miner to mine testnet. It also support merge mining.")
+        .arg(arg!(-c --config <FILE> "Specify configuation file.")
+                .value_parser(value_parser!(String))
+                .default_value("./miner.toml"),
+        )
+        .get_matches();
 
-    let config = configs::read_config();
+    let config_path = matches.get_one::<String>("config").expect("config file path to be specified or default to './miner.toml'");
+
+    let config = configs::read_config(config_path);
 
     // Verifying address is correct
     // TODO: verify length is 20 bytes (correct size for a pubkey hash)
